5295e62de363eedcb979c0fcc7641db2bce2c9ae
menna <menna.allah04@gmail.com>
2025-01-17 17:43:01 +0200

    make the shopping cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -73,6 +73,12 @@ document.querySelectorAll('.js-addtocart').forEach((button) => {
                 quantity: 1
             });
         }
-        console.log(cart)
+
+        let cartQuantity = 0;
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        })
+
+        document.querySelector('.cart-quantity').innerHTML = cartQuantity;
     });
 });
