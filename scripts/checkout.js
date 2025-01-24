a55c4b25fc434dca33ea56474d44e7a31b9ea7c7
menna <menna.allah04@gmail.com>
2025-01-24 03:59:45 +0200

    save the data of delivery date option then link it to hcode and generate html for that

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@ import { cart, removeItem, calculateCartQuantity, updateQuantity} from "../data/
 import { products } from "../data/products.js";
 import { formatPrice } from "./utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import { deliveryOptions } from "../data/deliveryOptions.js";
 
 let cartHTML= '';
 
@@ -14,11 +15,18 @@ cart.forEach((item) => {
         };
       });
       
+    let date;
+    deliveryOptions.forEach((option)=>{
+      if(option.id === item.deliveryOptionId){
+        date = option;
+      }
+    })
+    const deliveryDate = dayjs().add(date.deliveryDays,'days').format('dddd, MMMM D');
     
     cartHTML+=`
     <div class="cart-item-container js-cart-container-${matching.id}">
             <div class="delivery-date">
-              Delivery date: Tuesday, June 21
+              Delivery date: ${deliveryDate}
             </div>
 
             <div class="cart-item-details-grid">
@@ -51,52 +59,43 @@ cart.forEach((item) => {
                 <div class="delivery-options-title">
                   Choose a delivery option:
                 </div>
-                <div class="delivery-option">
-                  <input type="radio" checked
-                    class="delivery-option-input"
-                    name="delivery-option-${matching.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Tuesday, June 21
-                    </div>
-                    <div class="delivery-option-price">
-                      FREE Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matching.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Wednesday, June 15
-                    </div>
-                    <div class="delivery-option-price">
-                      $4.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matching.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Monday, June 13
-                    </div>
-                    <div class="delivery-option-price">
-                      $9.99 - Shipping
-                    </div>
-                  </div>
-                </div>
+                ${deliveryOptionshtml(matching,item)}
               </div>
+
             </div>
           </div>
     `;
     
     
 });
+
+function deliveryOptionshtml(matching,item){
+  let html ='';
+  deliveryOptions.forEach((deliveryOption) => {
+    const today = dayjs();
+    const deliveryDate = today.add(deliveryOption.deliveryDays,'days').format('dddd, MMMM D');
+    const deliveryPrice = deliveryOption.priceCents === 0 ? 'FREE' : `$${formatPrice(deliveryOption.priceCents)} -`;
+    const isChecked = deliveryOption.id === item.deliveryOptionId;
+
+    html += `
+    <div class="delivery-option">
+    <input type="radio" ${isChecked? 'checked' : ''}
+      class="delivery-option-input"
+      name="delivery-option-${matching.id}">
+    <div>
+      <div class="delivery-option-date">
+        ${deliveryDate}
+      </div>
+      <div class="delivery-option-price">
+        ${deliveryPrice} Shipping
+      </div>
+    </div>
+  </div>
+    `
+  })
+  return html;
+}
+
 
 document.querySelector('.order-summary').innerHTML = cartHTML;
 
@@ -144,5 +143,3 @@ document.querySelectorAll('.save-quantity-link').forEach((saveButton) => {
     updateCartQuantity();
   })
 })
-
-console.log(dayjs().add(7, 'days').format('dddd, MMMM D'))
