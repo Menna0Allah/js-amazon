8c57f7efa5497af01e14e16a1ff7f0065b772f0d
menna <menna.allah04@gmail.com>
2025-01-28 01:25:37 +0200

    Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,21 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if(!cart) {
-  cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId: '1'
-},{
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '2'
-}];
+loadFromStorage();
+
+export function loadFromStorage(){
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if(!cart) {
+    cart = [{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+  },{
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+  }];
+  }
 }
 
 function saveToStorage() {
@@ -18,13 +24,13 @@ function saveToStorage() {
 
 export function addToCart(productId){
     let matching = cart.find((item) => item.productId === productId);
-    const selectedQuantity = Number(document.querySelector(`.select-${productId}`).value);
+    // const selectedQuantity = Number(document.querySelector(`.select-${productId}`).value);
     if (matching) {
-      matching.quantity += selectedQuantity;
+      matching.quantity += 1; 
     } else {
       cart.push({
         productId: productId,
-        quantity: selectedQuantity,
+        quantity: 1,
         deliveryOptionId: '1', // default value when add new item
       });
     }
--- tests-jasmine/utils/moneyTest.js
+++ tests-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatPrice } from '../scripts/utils/money.js';
+import { formatPrice } from '../../scripts/utils/money.js';
 
 describe('test suite: formatCurrency', () => {
     it('converts cents into dollars', () => {
