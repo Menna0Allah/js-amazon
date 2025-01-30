3b871bfb1c1924b022fd6d8d53ba79f9444b3820
menna <menna.allah04@gmail.com>
2025-01-30 20:45:58 +0200

    create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummery } from "./checkout/orderSummary.js";
 import { renderPaymentSummery } from "./checkout/paymentSummery.js";
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 renderOrderSummery();
 renderPaymentSummery();
