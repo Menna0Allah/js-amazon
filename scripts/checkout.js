379ad8999a3db9b553361d13a31b70a06cc743db
menna <menna.allah04@gmail.com>
2025-02-01 22:02:39 +0200

    convert loadProducts from XHR to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,16 +1,12 @@
 import { renderOrderSummery } from "./checkout/orderSummary.js";
 import { renderPaymentSummery } from "./checkout/paymentSummery.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch} from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend.js'
 
 Promise.all([
-    new Promise((resolve) => {
-        loadProducts(() => {
-            resolve('hello');
-        });
-    }),
+    loadProductsFetch(),
     new Promise((resolve) => {
         loadCart(() => {
             resolve();
