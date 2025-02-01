f2bd105c2932a1c110ce791564f5aaf30bcbc5b5
menna <menna.allah04@gmail.com>
2025-02-01 21:41:13 +0200

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,48 @@
 import { renderOrderSummery } from "./checkout/orderSummary.js";
 import { renderPaymentSummery } from "./checkout/paymentSummery.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend.js'
 
-loadProducts(()=>{
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('hello');
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        })
+    })
+]).then((values) => {
+    console.log(values)
     renderOrderSummery();
     renderPaymentSummery();
 });
+
+
+// new Promise((resolve) => {
+//     loadProducts(() => {
+//         resolve('hello');
+//     });
+// }).then((value) => {
+//     console.log(value)
+//     return new Promise((resolve) => {
+//         loadCart(() => {
+//             resolve();
+//         })
+//     })
+// }).then(() => {
+//     renderOrderSummery();
+//     renderPaymentSummery();
+// })
+
+
+// loadProducts(()=>{
+//     loadCart(() => {
+//         renderOrderSummery();
+//         renderPaymentSummery();
+//     });
+// });
