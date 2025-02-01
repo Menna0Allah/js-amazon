64acf953547535499860ab4e54f7c1bfce49f3e9
menna <menna.allah04@gmail.com>
2025-02-01 13:32:29 +0200

    Add backend file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummery } from "./checkout/orderSummary.js";
 import { renderPaymentSummery } from "./checkout/paymentSummery.js";
 // import '../data/cart-class.js';
+import '../data/backend.js'
 
 renderOrderSummery();
 renderPaymentSummery();
